1b345170c2b2491e9e9ace48db3299c2313542de
aamkari <aamkari.official@gmail.com>
2026-07-15 23:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -438,3 +438,36 @@ craftRows.forEach((row) => {
    craftObserver.observe(row);
 
 });
+
+/*=========================================
+COLLECTIONS PAGE ANIMATION
+=========================================*/
+
+const collectionCards = document.querySelectorAll(".collection-card");
+
+const collectionObserver = new IntersectionObserver((entries) => {
+
+   entries.forEach((entry) => {
+
+       if (entry.isIntersecting) {
+
+           entry.target.classList.add("show-card");
+
+           collectionObserver.unobserve(entry.target);
+
+       }
+
+   });
+
+}, {
+
+   threshold: 0.2
+
+});
+
+collectionCards.forEach((card) => {
+
+   collectionObserver.observe(card);
+
+});
+
